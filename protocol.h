0f09b2cc62ccb5d2fa540abe1072680adda381db
sengsly <sengsly@gmail.com>
2018-03-10 11:05:37 +0700

    Add files via upload

---
+++ protocol.h
@@ -0,0 +1,21 @@
+#define SET_RTC_TIME    0x10
+#define GET_RTC_TIME    0x11
+#define SET_TMR_ON      0x12
+#define SET_TMR_OFF     0x13
+#define SET_CUR_LIM     0x14
+#define SET_VOL_LIM     0x15
+#define SET_NE_ID       0x16
+#define SET_RADIO_CH    0x17
+#define SET_NE_ADDR     0x18
+#define SET_GW_ADDR     0x19
+
+byte databuffer[40];
+struct data_struct{
+    unsigned int slave_address;
+    unsigned int master_address;
+    unsigned int crc;
+    unsigned int register_id;
+    unsigned int commandType;
+    unsigned int dataLength;
+    int data_pointer;
+};
